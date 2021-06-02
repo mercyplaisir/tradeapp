5d239cee64204dda67c2f2165c7bf9b8dbaa22fc
mercyplaisir <kirangimercyplaisir@gmail.com>
2021-06-02 22:02:24 +0200

    update

--- BinanceApi.py
+++ BinanceApi.py
@@ -9,6 +9,8 @@ import websocket
 
 
 
+
+
 from tools import Tool
 
 
@@ -20,9 +22,7 @@ functions in  this file:
                         - margin_balance_of
                         - order _quantity_of
                         - get_klines
-                        - price_study
-                        - minute15_trend
-                        - minute5_trend
+                        
                         - coin_for_trade
                         - get_coin_price
                         - coinPriceChange
@@ -87,11 +87,7 @@ class Binance:
             symbol=coin_to_trade, side=SIDE_SELL, type=ORDER_TYPE_MARKET, quantity=order_quantity)
 
 
-    #ecrire dans un fichier json
-
-
-
-
+ 
     def margin_balance_of(self, coin: str):
         """
         recuperer la balance d'un crypto
@@ -199,85 +195,9 @@ class Binance:
         return klines
 
 
-    def price_study(coin_to_trade: str, klines: pd.DataFrame, advanced: bool, coinPrice: float):
-        """
-        study made on klines(dataframe)
-        """
-        if not advanced:
-
-            a = []
-            for n in range(0, 3):
-                if float(klines.loc[n]['close_price']) > float(klines.loc[n]['SMA_20']) and float(klines.loc[n]['open_price']) > float(klines.loc[n]['SMA_20']):
-                    y = True
-                    a.append(y)
-                else:
-                    y = False
-                    a.append(y)
-            j = 0
-            for i in a:
-                if i == True:
-                    j += 1
-                else:
-                    break
-            if j == 4:
-                bool_answer = True
-            else:
-                bool_answer = False
-    #-------------------------------------------------------------------------------
-
-        elif advanced:
-            #SMA20                      close price du 1er bougie arriere    openprice du 2eme bougie arriere     openprice du 1er bougie arriere     close price du 2eme bougie arriere
-            if float(klines.loc[0]['SMA_20']) > float(klines.loc[1]['close_price']) > float(klines.loc[2]['open_price']) > float(klines.loc[1]['open_price']) >= float(klines.loc[2]['close_price']):
-                if float(klines.loc[2]['open_price']) <= Tool.percent_calculator(float(klines.loc[1]['close_price']), -2):
-                    bool_answer = True
-            else:
-                bool_answer = False
-
-        return bool_answer
+    
 
 
-    def minute15_trend(self,coin_to_trade: str, coinPrice: float):
-        """
-        tendance d'un crypto dans un timeframe de 1heure
-
-
-
-        """
-        up_trend = False
-        try:
-            klines = self.get_klines(coin_to_trade, '15m', '1 day')
-
-            up_trend = self.price_study(coin_to_trade, klines, False, coinPrice)
-
-            if up_trend:
-                print('uptrend for 15min')
-            else:
-                print('no trend for 15min')
-
-        except:
-            up_trend = False
-
-        return up_trend
-
-
-    def minute5_trend(self,coin_to_trade: str, coinPrice: float):
-        """
-        tendance d'un crypto dans un timeframe de 1heure
-        """
-
-        up_trend = False
-        try:
-            klines = self.get_klines(coin_to_trade, '5m', '1day')
-
-            up_trend = self.price_study(coin_to_trade, klines, True, coinPrice)
-
-            if up_trend:
-                print('uptrend for 5min')
-            else:
-                print('no trend for 5min')
-        except:
-            up_trend = False
-        return up_trend
 
 
     traded_crypto = []  # liste des crypto deja trader
@@ -375,6 +295,11 @@ class Binance:
                 json_result = was.recv()
                 was.close()
                 dict_result = json.loads(json_result)
+
+
+
+
+                
 
                 print('coinPrice')
                 break
--- strategies.py
+++ strategies.py
@@ -1,9 +1,118 @@
-"""
-create a class that herit form binance class so it can use it's method
+from BinanceApi import Binance
+import pandas as pd
+import json
+
+from tools import Tool
+
 
 """
-
+functions in this file:
+                        - price_study
+                        - minute15_trend
+                        - minute5_trend
+"""
 
 
 class Strategie:
+
+    def __init__(self):
+        self.client = Binance()
+        pass
+
+    
+
+
+    def minute15_trend(self, coin_to_trade: str, coinPrice: float):
+        """
+        tendance d'un crypto dans un timeframe de 1heure
+
+
+
+        """
+        up_trend = False
+        try:
+            klines = self.client.get_klines(coin_to_trade, '15m', '1 day')
+
+            up_trend = self.price_study(coin_to_trade, klines, False, coinPrice)
+
+            if up_trend:
+                print('uptrend for 15min')
+            else:
+                print('no trend for 15min')
+
+        except:
+            up_trend = False
+
+        return up_trend
+
+    def minute5_trend(self, coin_to_trade: str, coinPrice: float):
+        """
+        tendance d'un crypto dans un timeframe de 1heure
+        """
+
+        up_trend = False
+        try:
+            klines = self.client.get_klines(coin_to_trade, '5m', '1day')
+
+            up_trend = self.price_study(coin_to_trade, klines, True, coinPrice)
+
+            if up_trend:
+                print('uptrend for 5min')
+            else:
+                print('no trend for 5min')
+        except:
+            up_trend = False
+        return up_trend
+    pass    
+
+
+
+
+class Bollingerbands(Strategie):
+
+    def __init__(self):
+        super().__init__()
+
+    def price_study(self, coin_to_trade: str, klines: pd.DataFrame, advanced: bool, coinPrice: float):
+        """
+            study made on klines(dataframe)
+            """
+        if not advanced:
+
+            a = []
+            for n in range(0, 3):
+                if float(klines.loc[n]['close_price']) > float(klines.loc[n]['SMA_20']) and float(klines.loc[n]['open_price']) > float(klines.loc[n]['SMA_20']):
+                    y = True
+                    a.append(y)
+                else:
+                    y = False
+                    a.append(y)
+            j = 0
+            for i in a:
+                if i == True:
+                    j += 1
+                else:
+                    break
+            if j == 4:
+                bool_answer = True
+            else:
+                bool_answer = False
+        #-------------------------------------------------------------------------------
+
+        elif advanced:
+                  #SMA20                      close price du 1er bougie arriere    openprice du 2eme bougie arriere     openprice du 1er bougie arriere     close price du 2eme bougie arriere
+            if float(klines.loc[0]['SMA_20']) > float(klines.loc[1]['close_price']) > float(klines.loc[2]['open_price']) > float(klines.loc[1]['open_price']) >= float(klines.loc[2]['close_price']):
+                if float(klines.loc[2]['open_price']) <= Tool.percent_calculator(float(klines.loc[1]['close_price']), -2):
+                    bool_answer = True
+            else:
+                bool_answer = False
+
+        return bool_answer
+
+
+class Sma(Strategie):
+    pass
+
+
+class Macd(Strategie):
     pass
