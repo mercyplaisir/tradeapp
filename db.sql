e24b3d7510ee964ac74f89718cfffd75450f01b7
mercyplaisir <kirangimercyplaisir@gmail.com>
2021-07-19 00:16:53 +0200

    database

--- db.sql
+++ db.sql
@@ -1,9 +1,11 @@
 create database bot;
 
 use bot;
-create TABLE Trades(tradeid INT(15) PRIMARY KEY not null,
+create TABLE Trades(coinName VARCHAR(10) PRIMARY KEY,
                     crypto VARCHAR(20) not null,
-                    tradeTime TIMESTAMP not null);
+                    orderType VARCHAR(20),
+                    quantity FLOAT(20) NOT NULL,
+                    tradeTime DATETIME);
 
 
 create TABLE Coin(coinName VARCHAR(20) PRIMARY KEY not null,
@@ -14,3 +16,4 @@ create TABLE Coin(coinName VARCHAR(20) PRIMARY KEY not null,
 
 INSERT INTO Coin(coinName,fullName,marketCap)
 VALUES ('BTC','BITCOIN',0),('ETH','ETHEREUM',0),('BNB','BINANCE COIN',0),('DOGE','DOGECOIN',0);
+
