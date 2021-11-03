15c40fc5ef12443d6cb2e674d3daf5c7d149b871
mercyplaisir <kirangimercyplaisir@gmail.com>
2021-11-03 08:56:34 +0200

    update

---
+++ src/api/sensitive.py
@@ -0,0 +1,5 @@
+
+
+##for tradeapp project
+BINANCE_PUBLIC_KEY="eKDyjsVeMhssfXL89oil2keouZSfpnJwqJV3mfvApOYDylfUjGc6hKAtapQIHL3b"
+BINANCE_PRIVATE_KEY="hISw2v7P96RXq698sIQVUGHfhX3Jt8aqh9FOlURGfXFwelYKq1R5oPfUbfWtD9lo"
