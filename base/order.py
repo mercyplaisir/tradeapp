757e17ebf890fdf8cd664951b0286362ba634330
mercyplaisir <kirangimercyplaisir@gmail.com>
2022-03-06 14:00:06 +0200

    updates

--- base/order.py
+++ base/order.py
@@ -7,8 +7,8 @@ from dataclasses import dataclass,field
 
 # import requests
 from binance import BinanceSocketManager, AsyncClient
-import sys
-sys.path.append('..')
+# import sys
+# sys.path.append('..')
 from common import TAKE_PROFIT, percent_change, URL, HISTORY_ENDPOINT, send_data,track_order
 
 
@@ -69,62 +69,62 @@ class Order:
 
     
 
-d={
-  "symbol": "BTCUSDT",
-  "orderId": 28,
-  "orderListId": -1, #//Unless OCO, value will be -1
-  "clientOrderId": "6gCrw2kRUAF9CvJDGP16IP",
-  "transactTime": 1507725176595,
-  "price": "60000",
-  "origQty": "10.00000000",
-  "executedQty": "10.00000000",
-  "cummulativeQuoteQty": "10.00000000",
-  "status": "FILLED",
-  "timeInForce": "GTC",
-  "type": "MARKET",
-  "side": "SELL",
-  "fills": [
-    {
-      "price": "4000.00000000",
-      "qty": "1.00000000",
-      "commission": "4.00000000",
-      "commissionAsset": "USDT",
-      "tradeId": 56
-    },
-    {
-      "price": "3999.00000000",
-      "qty": "5.00000000",
-      "commission": "19.99500000",
-      "commissionAsset": "USDT",
-      "tradeId": 57
-    },
-    {
-      "price": "3998.00000000",
-      "qty": "2.00000000",
-      "commission": "7.99600000",
-      "commissionAsset": "USDT",
-      "tradeId": 58
-    },
-    {
-      "price": "3997.00000000",
-      "qty": "1.00000000",
-      "commission": "3.99700000",
-      "commissionAsset": "USDT",
-      "tradeId": 59
-    },
-    {
-      "price": "3995.00000000",
-      "qty": "1.00000000",
-      "commission": "3.99500000",
-      "commissionAsset": "USDT",
-      "tradeId": 60
-    }
-  ]
-}
+# d={
+#   "symbol": "BTCUSDT",
+#   "orderId": 28,
+#   "orderListId": -1, #//Unless OCO, value will be -1
+#   "clientOrderId": "6gCrw2kRUAF9CvJDGP16IP",
+#   "transactTime": 1507725176595,
+#   "price": "60000",
+#   "origQty": "10.00000000",
+#   "executedQty": "10.00000000",
+#   "cummulativeQuoteQty": "10.00000000",
+#   "status": "FILLED",
+#   "timeInForce": "GTC",
+#   "type": "MARKET",
+#   "side": "SELL",
+#   "fills": [
+#     {
+#       "price": "4000.00000000",
+#       "qty": "1.00000000",
+#       "commission": "4.00000000",
+#       "commissionAsset": "USDT",
+#       "tradeId": 56
+#     },
+#     {
+#       "price": "3999.00000000",
+#       "qty": "5.00000000",
+#       "commission": "19.99500000",
+#       "commissionAsset": "USDT",
+#       "tradeId": 57
+#     },
+#     {
+#       "price": "3998.00000000",
+#       "qty": "2.00000000",
+#       "commission": "7.99600000",
+#       "commissionAsset": "USDT",
+#       "tradeId": 58
+#     },
+#     {
+#       "price": "3997.00000000",
+#       "qty": "1.00000000",
+#       "commission": "3.99700000",
+#       "commissionAsset": "USDT",
+#       "tradeId": 59
+#     },
+#     {
+#       "price": "3995.00000000",
+#       "qty": "1.00000000",
+#       "commission": "3.99500000",
+#       "commissionAsset": "USDT",
+#       "tradeId": 60
+#     }
+#   ]
+# }
 
 
-order1=Order(**d)
-track_order(order=order1)
+# order1=Order(**d)
+# track_order(order=order1)
 # # order2 = Order(**d)
 # order1.save()
 # # Order.change()
--- test.py
+++ test.py
@@ -1,16 +1,63 @@
-import json
-from base import Coin
 
-def get_coin_data():
-    """just for view"""
-    all_coins = Coin.get_all_coins()
+from base import Order
+from common import track_order
 
-    data: dict = {coin.name: coin.get_cryptopair_related() for coin in all_coins}
 
-    data = {n: [str(l) for l in ll] for n, ll in data.items()}
 
-    nn = json.dumps(data)
-    with open("coindata.json", "w") as f:
-        f.write(nn)
 
-get_coin_data()
+d={
+  "symbol": "BTCUSDT",
+  "orderId": 28,
+  "orderListId": -1, #//Unless OCO, value will be -1
+  "clientOrderId": "6gCrw2kRUAF9CvJDGP16IP",
+  "transactTime": 1507725176595,
+  "price": "60000",
+  "origQty": "10.00000000",
+  "executedQty": "10.00000000",
+  "cummulativeQuoteQty": "10.00000000",
+  "status": "FILLED",
+  "timeInForce": "GTC",
+  "type": "MARKET",
+  "side": "SELL",
+  "fills": [
+    {
+      "price": "4000.00000000",
+      "qty": "1.00000000",
+      "commission": "4.00000000",
+      "commissionAsset": "USDT",
+      "tradeId": 56
+    },
+    {
+      "price": "3999.00000000",
+      "qty": "5.00000000",
+      "commission": "19.99500000",
+      "commissionAsset": "USDT",
+      "tradeId": 57
+    },
+    {
+      "price": "3998.00000000",
+      "qty": "2.00000000",
+      "commission": "7.99600000",
+      "commissionAsset": "USDT",
+      "tradeId": 58
+    },
+    {
+      "price": "3997.00000000",
+      "qty": "1.00000000",
+      "commission": "3.99700000",
+      "commissionAsset": "USDT",
+      "tradeId": 59
+    },
+    {
+      "price": "3995.00000000",
+      "qty": "1.00000000",
+      "commission": "3.99500000",
+      "commissionAsset": "USDT",
+      "tradeId": 60
+    }
+  ]
+}
+
+
+order1=Order(**d)
+track_order(order=order1)
