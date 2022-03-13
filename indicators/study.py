50fe5652bf31f8750303f846eac8de1807bf9f14
mercyplaisir <kirangimercyplaisir@gmail.com>
2022-03-13 13:33:43 +0200

    indicators improvements

--- indicators/study.py
+++ indicators/study.py
@@ -30,13 +30,23 @@ class Study:
             indicator.price_study(klines=klines) for indicator in indicators
         ]  # studying klines
 
-        approv_for_buy = (len(indicators) // 2) + 2 # the middle + 2
+        buy_count = study_list.count("buy")
+        sell_count = study_list.count("sell")
 
-        approv_for_sell = (len(indicators) // 2) + 1 # the middle + 1
+        # approv_for_buy = (len(indicators) // 2) + 2 # the middle + 2
 
-        if study_list.count("buy") >= approv_for_buy:
-            return ("buy", study_list.count("buy"))
-        elif study_list.count("sell") >= approv_for_sell:
-            return ("sell", study_list.count("sell"))
-        else:
-            return ("wait", 0)
+        # approv_for_sell = (len(indicators) // 2) + 1 # the middle + 1
+
+        # if study_list.count("buy") >= approv_for_buy:
+        #     return ("buy", study_list.count("buy"))
+        # elif study_list.count("sell") >= approv_for_sell:
+        #     return ("sell", study_list.count("sell"))
+        # else:
+        #     return ("wait", 0)
+        
+        if buy_count > sell_count:
+            return ("buy", buy_count)
+        elif sell_count < buy_count:
+            return ("sell", sell_count)
+        elif buy_count==sell_count:
+            return ("wait", sell_count)
