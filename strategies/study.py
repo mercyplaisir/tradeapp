4bf1c2d133cbc157a58fe279ad38aeab6a3a8c9c
mercyplaisir <kirangimercyplaisir@gmail.com>
2022-07-14 19:43:07 +0300

    strategies study

--- strategies/study.py
+++ strategies/study.py
@@ -2,9 +2,6 @@
 from typing import Callable, Literal
 
 import pandas as pd
-# from strategies.strategies import BbRsi
-# from strategies.strategies.base import Strategie
-
 from strategies.strat import rsi7
 
 choosen_strategie = rsi7
@@ -14,16 +11,5 @@ def study(klines: pd.DataFrame, strategie:Callable = choosen_strategie) -> Liter
     return given_strategie_decision
 
 
-"""class Study:
-    \"""Class for Study\"""
-
-    # ==========Decision================
-    @classmethod
-    def decision(cls, klines: pd.DataFrame, strategie:Callable = choosen_strategie) -> Literal['buy','sell','wait']:  # cryptopair: str):
-        \"""For getting a decision from a given strategie\"""
-
-        given_strategie_decision:str = strategie(data=klines)
-        return given_strategie_decision
-"""
 
         
