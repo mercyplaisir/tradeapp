05016db87d8f6b360dacf43d246609413b516900
mercyplaisir <kirangimercyplaisir@gmail.com>
2023-04-26 17:24:22 +0300

    move funcs from tools to cryptopair

--- tradeapp/cryptopair.py
+++ tradeapp/cryptopair.py
@@ -4,9 +4,10 @@ represent pair
 from typing import Dict, List,Protocol,Any, Self
 
 import pandas as pd
+import numpy as np
 
 from tradeapp.protocols import Exchange
-from tradeapp.tools import OrderType
+from tradeapp.tools import OrderType, Timeframe ,Trend
 
 class Crypto:
     def __init__(self,name:str,ex: Exchange) -> None:
@@ -104,7 +105,7 @@ class CryptoPair:
             amount = round(amount,self.quoteAssetPrecision)
         )
     
-    def get_ohlc(self,timeframe:str):
+    def get_ohlc(self,timeframe = Timeframe.DAY):
         data = self.exchange.fetch_ohlcv(
         symbol = f'{self.get_baseAsset()}/{self.get_quoteAsset()}',
         timeframe= timeframe
@@ -131,3 +132,69 @@ class CryptoPair:
 
     def __str__(self) -> str:
         return f"looking at {self.get_symbol()} \n "
+    
+    def get_support_and_resistance(self, timeframe = Timeframe.DAY) -> List[Tuple[int,str]]:
+        """return resistance and support on given data
+
+        Args:
+            df (pd.DataFrame): contains ohlc of given crypto
+        """
+        #method 1: fractal candlestick pattern
+        # determine bullish fractal 
+        df:pd.DataFrame = self.get_ohlc(timeframe = timeframe)
+        def is_support(df,i):  
+            cond1 = df['Low'][i] < df['Low'][i-1]   
+            cond2 = df['Low'][i] < df['Low'][i+1]   
+            cond3 = df['Low'][i+1] < df['Low'][i+2]   
+            cond4 = df['Low'][i-1] < df['Low'][i-2]  
+            return (cond1 and cond2 and cond3 and cond4) 
+        # determine bearish fractal
+        def is_resistance(df,i):  
+            cond1 = df['High'][i] > df['High'][i-1]   
+            cond2 = df['High'][i] > df['High'][i+1]   
+            cond3 = df['High'][i+1] > df['High'][i+2]   
+            cond4 = df['High'][i-1] > df['High'][i-2]  
+            return (cond1 and cond2 and cond3 and cond4)
+        # to make sure the new level area does not exist already
+        def is_far_from_level(value, levels, df):    
+            ave =  np.mean(df['High'] - df['Low'])    
+            return np.sum([abs(value-level)<ave for _,level in levels])==0
+        # a list to store resistance and support levels
+        levels = []
+        high_low = {
+        'highs': [],
+        'lows' : []
+        }
+        for i in range(2, df.shape[0] - 2):  
+            if is_support(df, i):    
+                low = df['Low'][i]  
+                
+                if is_far_from_level(low, levels, df):      
+                    levels.append((i, low))  
+                    high_low['lows'].append(low)
+            elif is_resistance(df, i):    
+                high = df['High'][i]
+                if is_far_from_level(high, levels, df):      
+                    levels.append((i, high))
+                    high_low['highs'].append(high) 
+        return levels
+    def get_trend(self,timeframe = Timeframe.DAY) -> Trend:
+        """give the trend of the current stock
+
+        Args:
+            df (pd.DataFrame): contains ohlc data of given crypto
+        """
+        #data
+        df = self.get_ohlc(timeframe = timeframe)
+        sma_size = 200
+        # Get the trend of the market by using sma200
+        # Get list of 5 last closed price
+        price : List[float] = df['Close'][-5:].to_list()
+        # calculate sma 
+        df[f'SMA_{sma_size}'] = df['Close'].rolling(window=sma_size).mean()
+        sma_value : List[float] = df[f'SMA_{sma_size}'][-5:].to_list()
+        # condition
+        cond = price > sma_value
+        if cond:
+            return Trend.UPTREND
+        return Trend.DOWNTREND
